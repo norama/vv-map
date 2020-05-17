33701d08783ddcacb4c062338c2d506dcb7a7c0b
Nóra Máté <norama.matema@gmail.com>
2020-05-17 17:58:38 +0200

    minor

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Button } from 'reactstrap';
 
 import { format } from 'date-fns';
 
