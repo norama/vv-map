5c525d9ffbeb0445daced7a1072e636cec008c4e
Nóra Máté <norama.matema@gmail.com>
2020-06-03 23:39:27 +0200

    ExternalVirusInfo name map

--- src/components/info/ExternalVirusInfo.js
+++ src/components/info/ExternalVirusInfo.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 import './ExternalVirusInfo.css';
 
+const MAP = {
+    Czechia: 'czech-republic',
+    'S. Korea': 'south-korea'
+};
+
+function countryMap(country) {
+    return MAP[country] ? MAP[country] : country;
+}
+
 const ExternalVirusInfo = ({ country, province }) => (
     <div className="__CountryVirusInfo__">
         <a href={country === 'USA' ?
             process.env.REACT_APP_VIRUS_USA_REFERENCE_BASE_URL+'/'+province : 
-            process.env.REACT_APP_VIRUS_COUNTRY_REFERENCE_BASE_URL+'/'+country}
+            process.env.REACT_APP_VIRUS_COUNTRY_REFERENCE_BASE_URL+'/'+countryMap(country)}
             target="_blank" rel="noopener noreferrer"
         >
             {country === 'USA' ? province : country} virus data
