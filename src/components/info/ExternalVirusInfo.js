45d9872ce0a3f93f0eda676d47db17c2db45c1d8
Nóra Máté <norama.matema@gmail.com>
2020-06-24 22:34:35 +0200

    New York province external link fix

--- src/components/info/ExternalVirusInfo.js
+++ src/components/info/ExternalVirusInfo.js
@@ -3,20 +3,28 @@ import PropTypes from 'prop-types';
 
 import './ExternalVirusInfo.css';
 
-const MAP = {
+const COUNTRY_MAP = {
     Czechia: 'czech-republic',
     'S. Korea': 'south-korea',
     USA: 'US'
 };
 
+const PROVINCE_MAP = {
+    'New York City, NY': 'new-york'
+};
+
 function countryMap(country) {
-    return MAP[country] ? MAP[country] : country;
+    return COUNTRY_MAP[country] ? COUNTRY_MAP[country] : country;
+}
+
+function provinceMap(province) {
+    return PROVINCE_MAP[province] ? PROVINCE_MAP[province] : province;
 }
 
 const ExternalVirusInfo = ({ country, province }) => (
     <div className="__CountryVirusInfo__">
         <a href={country === 'USA' && province ?
-            process.env.REACT_APP_VIRUS_USA_REFERENCE_BASE_URL+'/'+province : 
+            process.env.REACT_APP_VIRUS_USA_REFERENCE_BASE_URL+'/'+provinceMap(province) :
             process.env.REACT_APP_VIRUS_COUNTRY_REFERENCE_BASE_URL+'/'+countryMap(country)}
             target="_blank" rel="noopener noreferrer"
         >
